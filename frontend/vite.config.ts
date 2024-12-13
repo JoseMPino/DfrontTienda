81a1df3329641aa658100321d1d1ee6f39ca5250
JoseMPino <j2005mpv@gmail.com>
2024-12-13 14:48:59 -0500

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 4173, // Usa el puerto proporcionado por Render o el puerto por defecto
+    host: '0.0.0.0', // Asegúrate de que el servidor esté escuchando en todas las interfaces de red
+  }
 })
+
